4bde4d00a9a035cdf326189fe98c049f926152ee
reillyp6 <32643502+reillyp6@users.noreply.github.com>
2017-10-16 16:17:44 +0100

    Add files via upload
    Basic skeleton code written, tests to be done

--- BinaryLCA.java
+++ BinaryLCA.java
@@ -16,5 +16,25 @@ public class BinaryLCA {
 
 	Node root;
 	
+	Node findLCA(int n1, int n2)
+	    {
+	        return findLCA(root, n1, n2);
+	    }
 	
+	Node findLCA(Node node, int n1, int n2)
+    {
+		 if (node == null)
+			 return null;
+	 
+	     if (node.value == n1 || node.value == n2)
+	    	 return node;
+	
+	     Node left_lca = findLCA(node.left, n1, n2);
+	     Node right_lca = findLCA(node.right, n1, n2);
+	
+	     if (left_lca!=null && right_lca!=null)
+	    	 return node;
+	 
+	     return (left_lca != null) ? left_lca : right_lca;
+    }
 }
