e651ca671d9b2a3669cab94c34320bd619c19d69
reillyp6 <32643502+reillyp6@users.noreply.github.com>
2017-10-16 16:33:11 +0100

    Add files via upload
    Initial test written, findLCA

--- BinaryLCATest.java
+++ BinaryLCATest.java
@@ -7,6 +7,15 @@ public class BinaryLCATest {
 	@Test
 	public void findLCATest() {
 		
+		BinaryLCA tree = new BinaryLCA();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.right.right = new Node(4);
+		tree.root.right.left = new Node(5);
+		
+		assertEquals("Test findLCA method works correctly",3,tree.findLCA(4,5).value);
+		
 	}
 	
 	
