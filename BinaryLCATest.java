3faf1d9ce9db98e97d100ca41b75e5d7305da9cc
reillyp6 <32643502+reillyp6@users.noreply.github.com>
2017-10-18 14:16:18 +0100

    Add files via upload
    more advanced implementation, additional test classes

--- BinaryLCATest.java
+++ BinaryLCATest.java
@@ -7,36 +7,39 @@ public class BinaryLCATest {
 	@Test
 	public void findLCATest1() {
 		
-		BinaryLCA tree = new BinaryLCA();
-		tree.root = new Node(1);
-		tree.root.left = new Node(2);
-		tree.root.right = new Node(3);
-		tree.root.right.right = new Node(4);
-		tree.root.right.left = new Node(5);
 		
-		assertEquals("Test findLCA method works correctly",3,tree.findLCA(4,5).value);	
-	}
-	
-	@Test
-	public void overrideDataTest()
-	{
-		BinaryLCA tree = new BinaryLCA();
-		tree.root = new Node(1);
-		tree.root.left = new Node(2);
-		tree.root.right = new Node(3);
-		tree.root.right = new Node(6);
-		tree.root.left = new Node (4);
-		
-		assertEquals("Test findLCA works correcty after overriding data in tree", 1, tree.findLCA(6,4).value);
 	}
 	
 	@Test
 	public void testNullCase()
 	{
-		BinaryLCA tree = new BinaryLCA();
-		
-		assertNull("Test method returns null for an empty tree", tree.findLCA(1,2));
+
 	}
+	
+	public void testPut() 
+	 {
+		 
+	 }
+	 
+	 @Test
+	 public void testGet()
+	 {
+		 
+	 }
+	 
+	 @Test
+	 public void testContains()
+	 {
+		 
+	 }
+	 
+	 
+	 @Test 
+	 public void testEmpty()
+	 {
+		
+		 
+	 }
 	
 	
 	
