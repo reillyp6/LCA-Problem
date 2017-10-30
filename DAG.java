44388979db0633a01b4e9a7faed77eda15bb0517
reillyp6 <32643502+reillyp6@users.noreply.github.com>
2017-10-30 14:31:09 +0000

    Add files via upload
    AddEdge and GetNode methods added with working test classes, LCA method still not implemented

--- DAG.java
+++ DAG.java
@@ -38,12 +38,54 @@ public class  DAG
 	    
 	    public void addEdge(int id1, int id2)
 	    {
-	    	
+	    	boolean node1Found = false, node2Found = false;
+	        Node node1  = null, node2 = null;
+	        
+	        for (int i = 0; i < nodeList.size(); i++)
+	        {
+	            if (nodeList.get(i).nodeId == id1)
+	            {
+	                node1Found = true;
+	                node1 = nodeList.get(i);
+	            }
+	            if (nodeList.get(i).nodeId == id2)
+	            {
+	                node2Found = true;
+	                node2 = nodeList.get(i);
+	            }
+	            if (node1Found && node2Found) break;
+	        }
+	        
+	        
+	        if (!node1Found)
+	        {
+	            node1 = this.addNode(id1); 
+	        }
+	        
+	        if (!node2Found)
+	        {
+	            node2 = this.addNode(id2); 
+	        }
+	        
+	        Node temp = new Node(id2);
+	        temp.next = node1.next;
+	        node1.next = temp;
+	        
+	        return;
 	    }
 	    
 	    public Node getNode(int id)
 	    {
-	    	
+	    	for (int i = 0; i < nodeList.size(); i++)
+	        {
+	            if (id == nodeList.get(i).nodeId)
+	            {
+	                return nodeList.get(i);
+	            }
+	            
+	        }
+	        
+	        return null;
 	    }
 	    
 	    public boolean contains(int id)
--- DAGTest.java
+++ DAGTest.java
@@ -32,18 +32,50 @@ public class DAGTest {
 	@Test
 	public void addEdgeTest()
 	{
+		DAG dag = new DAG();
 		
+		dag.addEdge(1, 2);
+		
+		assertTrue("Test method works correctly for empty graph",(dag.contains(1)));
+		assertTrue("Test method works correctly for empty graph",(dag.contains(2)));
+		
+		assertEquals(2, dag.nodeList.get(1).next.nodeId);
+		
+		dag.addNode(3);
+		dag.addNode(4);
+		
+		dag.addEdge(1, 3);
+		dag.addEdge(3, 4);
+		
+		assertEquals("test method works when both nodes are in graph",4, dag.nodeList.get(1).next.nodeId);
+		
+		dag.addEdge(4,7);
+		
+		assertEquals("Test method works when one of the mentioned nodes is in graph",7,dag.nodeList.get(1).next.nodeId);
 	}
 	
 	@Test
 	public void getNodeTest()
 	{
+		DAG dag = new DAG();
 		
+		dag.addEdge(1, 2);
+		dag.addEdge(2, 4);
+		
+		assertEquals("Test getNode method works correctly", 1, dag.getNode(1).nodeId);
 	}
 	
 	@Test
 	public void findLCATest()
 	{
+		DAG dag = new DAG();
+		
+		dag.addEdge(1, 2);
+		dag.addEdge(2, 4);
+		dag.addEdge(1, 3);
+		dag.addEdge(3, 5);
+		
+		assertEquals("Test method works correctly",3, findLCA(3, 5).nodeId);
 		
 	}
 }
