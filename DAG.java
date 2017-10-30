7afcbff086b04cd4aee20432754142f4d42f3b17
reillyp6 <32643502+reillyp6@users.noreply.github.com>
2017-10-30 14:07:15 +0000

    Add files via upload
    Basic API and test class for finding LCA for a directed acyclic graph

---
+++ DAG.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class  DAG
+	{
+	    public class Node
+	    {
+	        int nodeId;
+	        Node next;
+	        
+	        public Node(int id)
+	        {
+	            this.nodeId = id;
+	        }
+	    }
+	    
+	    ArrayList<Node> nodeList;
+	    
+	    public DAG()
+	    {
+	        nodeList = new ArrayList<Node>();
+	    }
+	    
+	    public Node findLCA (int id1, int id2)
+	    {
+	    	
+	    }
+	    
+	    public Node addNode(int id)
+	    {
+	    	
+	    }
+	    
+	    
+	    public void addEdge(int id1, int id2)
+	    {
+	    	
+	    }
+	    
+	    public Node getNode(int id)
+	    {
+	    	
+	    }
+	    
+	    public boolean contains(int id)
+	    {
+	    	
+	    }
+	}
+  
---
+++ DAGTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DAGTest {
+
+	@Test
+	public void addNodeTest() 
+	{
+		
+	}
+	
+	@Test
+	public void containsTest()
+	{
+		
+	}
+	
+	@Test
+	public void addEdgeTest()
+	{
+		
+	}
+	
+	@Test
+	public void getNodeTest()
+	{
+		
+	}
+	
+	@Test
+	public void findLCATest()
+	{
+		
+	}
+}
