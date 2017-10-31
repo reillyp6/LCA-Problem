0e983d358ae4a81e510d3c78689c6de0eaea7a7d
reillyp6 <32643502+reillyp6@users.noreply.github.com>
2017-10-31 16:02:41 +0000

    Add files via upload
    Complete re-work as node class was proving too difficult to work with. Based on code found online for similar problem.

--- DAG.java
+++ DAG.java
@@ -1,99 +1,84 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 
-import DAG_LCA.Node;
+public class DAG {
+	private int vert;           
+	private int edge;                 
+	private ArrayList<Integer>[] adj;    
+	private int[] indegree;        
+	private boolean visited[];		
+	private boolean cyclic;		
+    private boolean stack[];		
 
-public class  DAG
-	{
-	    public class Node
-	    {
-	        int nodeId;
-	        Node next;
-	        
-	        public Node(int id)
-	        {
-	            this.nodeId = id;
-	        }
-	    }
-	    
-	    ArrayList<Node> nodeList;
-	    
-	    public DAG()
-	    {
-	        nodeList = new ArrayList<Node>();
-	    }
-	    
-	    public Node findLCA (int id1, int id2)
-	    {
-	    	
-	    }
-	    
-	    public Node addNode(int id)
-	    {
-	    	Node node = new Node(id);
-	        nodeList.add(0, node);
 	
-	        return nodeList.get(0);
-	    }
-	    
-	    
-	    public void addEdge(int id1, int id2)
-	    {
-	    	boolean node1Found = false, node2Found = false;
-	        Node node1  = null, node2 = null;
-	        
-	        for (int i = 0; i < nodeList.size(); i++)
-	        {
-	            if (nodeList.get(i).nodeId == id1)
-	            {
-	                node1Found = true;
-	                node1 = nodeList.get(i);
-	            }
-	            if (nodeList.get(i).nodeId == id2)
-	            {
-	                node2Found = true;
-	                node2 = nodeList.get(i);
-	            }
-	            if (node1Found && node2Found) break;
-	        }
-	        
-	        
-	        if (!node1Found)
-	        {
-	            node1 = this.addNode(id1); 
-	        }
-	        
-	        if (!node2Found)
-	        {
-	            node2 = this.addNode(id2); 
-	        }
-	        
-	        Node temp = new Node(id2);
-	        temp.next = node1.next;
-	        node1.next = temp;
-	        
-	        return;
-	    }
-	    
-	    public Node getNode(int id)
-	    {
-	    	for (int i = 0; i < nodeList.size(); i++)
-	        {
-	            if (id == nodeList.get(i).nodeId)
-	            {
-	                return nodeList.get(i);
-	            }
-	            
-	        }
-	        
-	        return null;
-	    }
-	    
-	    public boolean contains(int id)
-	    {
-	    	if(getNode(id)== null)
-	    		return false;
-	    	
-	    	return true;
-	    }
+	public DAG(int vert)
+	{
+		   
 	}
-  
+	
+	public void addEdge(int a, int b)
+	{
+	    	
+	}
+
+	public int vert() 
+	{
+		return vert;	
+	}
+	
+	public int edge() 
+	{
+        return edge;
+    }
+	
+	private int checkVert(int v)
+	{
+        
+    }
+
+	public int indegree(int v)
+	{
+		
+	}
+	
+	public int outdegree(int v)
+	{
+		
+    }
+		
+	public Iterable<Integer> adj(int v)
+	{ 
+		return adj[v]; 
+	}
+
+	public boolean isCyclic() 
+	{
+
+        return cyclic;
+    }
+	
+	 public void findCycle(int v) 
+	 {
+
+	        
+	 }
+	 
+	public int findLCA(int v, int w)
+	{
+		
+	}
+	
+	
+	
+    public ArrayList<Integer> BFS(int s)
+    {
+       
+        
+    }
+    
+    public DAG reverse() 
+    {
+       
+    }
+}
--- DAGTest.java
+++ DAGTest.java
@@ -1,81 +1,49 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class DAGTest {
+	
+	@Test
+	public void testDiGraph()
+	{
+	
+	}
+	
+	
+	@Test
+	public void testAddEdge()
+	{
+
+	}
+	
+	@Test
+	public void testinDegree()
+	{
+		
+	}
+	
+	@Test
+	public void testOutDegree()
+	{
+			
+	}
+	
+	@Test
+	public void testIsCyclic()
+	{
+		
+	}
+	
+	@Test
+	public void testFindCycle()
+	{
+			
+	}
 
 	@Test
-	public void addNodeTest() 
+	public void testFindLCA() 
 	{
-		DAG dag = new DAG();
 		
-		assertEquals("Test addNode method works correctly",7,dag.addNode(7).nodeId);
-		assertEquals("Test addNode method works correctly",9,dag.addNode(9).nodeId);
-
 	}
 	
-	@Test
-	public void containsTest()
-	{
-		DAG dag = new DAG();
-		
-		assertFalse("Test method works for empty graph",dag.contains(2));
-		
-		dag.addNode(3);
-		dag.addNode(4);
-		
-		dag.addEdge(1, 3);
-		
-		assertTrue("Test method works correctly for non-empty",dag.contains(1));
-	}
-	
-	@Test
-	public void addEdgeTest()
-	{
-		DAG dag = new DAG();
-		
-		dag.addEdge(1, 2);
-		
-		assertTrue("Test method works correctly for empty graph",(dag.contains(1)));
-		assertTrue("Test method works correctly for empty graph",(dag.contains(2)));
-		
-		assertEquals(2, dag.nodeList.get(1).next.nodeId);
-		
-		dag.addNode(3);
-		dag.addNode(4);
-		
-		dag.addEdge(1, 3);
-		dag.addEdge(3, 4);
-		
-		assertEquals("test method works when both nodes are in graph",4, dag.nodeList.get(1).next.nodeId);
-		
-		dag.addEdge(4,7);
-		
-		assertEquals("Test method works when one of the mentioned nodes is in graph",7,dag.nodeList.get(1).next.nodeId);
-	}
-	
-	@Test
-	public void getNodeTest()
-	{
-		DAG dag = new DAG();
-		
-		dag.addEdge(1, 2);
-		dag.addEdge(2, 4);
-		
-		assertEquals("Test getNode method works correctly", 1, dag.getNode(1).nodeId);
-	}
-	
-	@Test
-	public void findLCATest()
-	{
-		DAG dag = new DAG();
-		
-		dag.addEdge(1, 2);
-		dag.addEdge(2, 4);
-		dag.addEdge(1, 3);
-		dag.addEdge(3, 5);
-		
-		assertEquals("Test method works correctly",3, findLCA(3, 5).nodeId);
-		
-	}
 }
